8752c4043924ad61643fbc7898edf2d1304c34e9
Femo Bayani <festbawi@gmail.com>
2019-02-23 17:34:38 +0800

    Started using Pipeline, added without/with laplace smoothing for gen vocab, better output.

--- learn.py
+++ learn.py
@@ -1,6 +1,9 @@
+from sklearn import metrics
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.feature_selection import chi2, SelectKBest
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
 from time import gmtime, strftime, time
 import os
 import numpy as np
@@ -10,61 +13,47 @@ dataset_path = 'dataset/trec07p/'
 csv_path = 'processed-2019-02-21-00-50-30.csv'
 
 
-def time_taken(task, time_start):
-    time_taken = strftime('%H:%M:%S', gmtime(time() - time_start))
-    print('{} took {}'.format(task, time_taken))
+def learn(x_train, y_train):
+    gen_vocab_model = Pipeline([
+        ('vect', CountVectorizer()),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultinomialNB(alpha=1.0e-10))
+    ])
+    gen_vocab_model.fit(x_train, y_train)
+
+    gen_vocab_laplace_model = Pipeline([
+        ('vect', CountVectorizer()),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultinomialNB(alpha=1))
+    ])
+    gen_vocab_laplace_model.fit(x_train, y_train)
+
+    return gen_vocab_model, gen_vocab_laplace_model
 
 
-def learn(path):
-    main_t0 = time()
-
-    # Read file
-    t0 = time()
-    df = pd.read_csv(path, header=0, index_col=0, converters={
-                     'words': lambda x: ' '.join(x.strip('[]').replace('\'', '').split(', '))})
-    time_taken('File reading and concatenation', t0)
-
-    # Concatenate tokens
-    # t0 = time()
-    # df['text'] = df['words'].apply(' '.join)
-    # time_taken('Text concatenation', t0)
-
-    # https://stackabuse.com/the-naive-bayes-algorithm-in-python-with-scikit-learn/
-    t0 = time()
-    count_vect = CountVectorizer()
-    counts = count_vect.fit_transform(df['words'])
-    time_taken('CountVectorizer fit_transform', t0)
-
-    # Term Frequency Inverse Document Frequency
-    t0 = time()
-    transformer = TfidfTransformer().fit(counts)
-    counts = transformer.transform(counts)
-    time_taken('TfidfTransformer fit & transform', t0)
-
-    # Split data
-    t0 = time()
-    x_train, x_test, y_train, y_test = train_test_split(
-        counts, df['is_spam'], test_size=0.2)
-    time_taken('train_test_split', t0)
-
-    # Generate model
-    t0 = time()
-    model = MultinomialNB().fit(x_train, y_train)
-    time_taken('MultinomialNB fit', t0)
-
-    time_taken('Learning process', main_t0)
-
-    return model, x_test, y_test
-
-
-def evaluate(model, x_test, y_test):
+def evaluate(model, model_name, x_test, y_test, y_names):
+    print(model_name)
     predicted = model.predict(x_test)
-
-    accuracy = np.mean(predicted == y_test)
-
-    print('\nAccuracy: {}'.format(accuracy))
+    print('Accuracy: {:.05%}'.format(np.mean(predicted == y_test)))
+    print(metrics.classification_report(
+        y_test, predicted, target_names=y_names))
 
 
 if __name__ == '__main__':
-    model, x_test, y_test = learn(os.path.join(dataset_path, csv_path))
-    evaluate(model, x_test, y_test)
+    # Read file
+    df = pd.read_csv(os.path.join(dataset_path, csv_path),
+                     header=0, index_col=0)
+
+    # Split data
+    target_names = ['ham', 'spam']
+    x_train, x_test, y_train, y_test = train_test_split(
+        df['words'], df['is_spam'], test_size=0.2, random_state=191)
+
+    # Train models
+    gvoc_model, gvoclap_model = learn(x_train, y_train)
+
+    # Evaluate models
+    evaluate(gvoc_model, 'General Vocabulary (No laplace smoothing; alpha=0.0000000001≈0)',
+             x_test, y_test, target_names)
+    evaluate(gvoclap_model, 'General Vocabulary (With laplace smoothing; alpha=1)',
+             x_test, y_test, target_names)
